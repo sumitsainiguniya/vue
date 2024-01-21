07fb32fd411a01d1c9847057f135dff46e4c37e2
Sumit Saini <44585428+sumitsainiguniya@users.noreply.github.com>
2024-01-21 11:09:05 +0000

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+//import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld2 from './components/HelloWorld2.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <HelloWorld />
+  <HelloWorld2 msg="You did it!" />
 </template>
 
 <style scoped>
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,20 @@
+<template>
+  {{ msg }}
+  {{ hello }}
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount, onMounted, ref } from 'vue'
+defineProps<{
+  msg: string
+}>()
+const hello = ref('')
+onBeforeMount(() => {
+  hello.value = 'Sumit'
+  console.log('onBeforeMount', hello.value)
+})
+onMounted(() => {
+  hello.value = 'Amit'
+  console.log('onMounted', hello.value)
+})
+</script>
